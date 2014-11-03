3c12742bdf88d30dbdec6f2ad38b29aab0fd22dd
dlashrus <davidlashlee@gmail.com>
2014-11-03 14:09:28 +0800

    [FEATURE] make list controller

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'twitter-bootstrap-rails'
 
 gem "therubyracer"
 gem "less-rails"
+gem 'bootstrap_form'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,48 @@
+class ListsController < ApplicationController
+	def index
+		@lists = List.all
+	end
+
+	def new
+		@list = List.new
+	end
+
+	def create
+		@list = List.new(list_params)
+		
+
+		@list.save
+		redirect_to @list
+	end
+
+	def edit
+		@list = List.find(params[:id])
+	end
+
+	def update
+		@list = List.find(params[:id])
+
+		if @list.update(list_params)
+			redirect_to @list
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+
+	def destroy
+		@lists = List.find(params[:id])
+		@lists.destroy
+
+	redirect_to '/lists/new', :notice => "Your list has been deleted"
+	end
+
+	private
+	def list_params
+		params.require(:list).permit(:name)
+	end
+
+end
