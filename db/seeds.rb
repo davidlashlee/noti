203e496647dfd9bffe1d51b99cc2e79d4de19d7d
dlashrus <davidlashlee@gmail.com>
2014-11-26 17:35:47 +1300

    [HEROKU] adding data to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+	if User.all.count == 0
+	default = User.create({ email: 'test@test.com', { password: "testtest"}})
+	default.save
+	end 
